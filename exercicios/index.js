cbb0448cd0c6e0d044e228358c6583e66ece0c36
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2022-06-15 20:00:20 -0300

    style: remove comments

--- exercicios/index.js
+++ exercicios/index.js
@@ -4,8 +4,5 @@
 
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import App from './src/App';
-// import { name as appName } from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
 
 registerRootComponent(App);
