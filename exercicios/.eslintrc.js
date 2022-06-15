71c86d13a0c52dd81a3d61f0c02c9365aa56e39f
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2022-06-15 09:19:24 -0300

    chore: adjust linting config

--- exercicios/.eslintrc.js
+++ exercicios/.eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: '@react-native-community',
+    rules: {
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1,
+            },
+        ],
+    },
 };
